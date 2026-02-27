4788fcb20b052c28282d0f9c566a3f5f9a681caa
Padin358 <padin358@email.cz>
2026-02-27 21:44:45 +0100

    added wikiNew, Jáno se musí naučit Nuxt

---
+++ wikiNew/app/app.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="w-full h-16 bg-gray-800 text-white flex items-center justify-center gap-8">
+    <NuxtLink to="/" class="hover:underline">Home</NuxtLink>
+    <NuxtLink to="/wiki/" class="hover:underline">Wiki</NuxtLink>
+  </div>
+  <NuxtPage />
+</template>
+ 
---
+++ wikiNew/app/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <!-- <NuxtRouteAnnouncer />
+        <NuxtWelcome /> -->
+        <p class="text-center text-gray-500 text-sm mt-4 bg-blue-100 p-2 rounded-xl w-96 mx-auto">
+        <a href="https://nuxt.com/modules/tailwindcss" target="_blank" class="text-blue-500 hover:underline">Tailwind CSS</a>
+        is ready to use in your Nuxt 4 project.
+        </p>
+    </div>
+    <NuxtPage />
+</template>
